cbc2dec260324607c8aa1feea521f8f01e009665
PinkTrout846771 <aaravd0316@gmail.com>
2020-12-19 01:12:35 -0500

    Still javaApplet

--- javaApplet.java
+++ javaApplet.java
@@ -6,10 +6,6 @@ public class igAppletFirst extends Applet {
 	
 	public static void main(String[] args) {
 		
-		JFrame frame = new JFrame();
-		frame.setSize(100,100);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Example Frame");
-		frame.setVisible(true);
+		System.out.println("hecklekeklol");
 	}
 }
